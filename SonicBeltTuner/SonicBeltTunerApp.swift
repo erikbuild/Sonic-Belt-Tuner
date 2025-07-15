05550295abc3c7d57adf36e816d3c850e70070c6
Erik Reynolds <erik@reynolds.io>
2025-07-15 15:44:11 -0400

    Initial Commit

---
+++ SonicBeltTuner/SonicBeltTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonicBeltTunerApp.swift
+//  SonicBeltTuner
+//
+//  Created by Erik Reynolds on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SonicBeltTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
